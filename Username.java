e8a6c1a3652401580cfb32ad4ec6cfc10797bb33
deekshitha-puppala <puppaladeekshitha2@gmail.com>
2025-07-05 00:31:14 +0530

    Add files via upload

---
+++ Username.java
@@ -0,0 +1,17 @@
+// DAY-1
+//USERNAME GENERATOR
+import java.util.*;
+public class Username {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your nick name: ");
+        String nick_name = sc.nextLine();
+        System.out.print("Enter your real name: ");
+        String user_name = sc.nextLine();
+        System.out.print("your user_name is: \"" + nick_name + "_" + user_name +"\"");
+
+
+
+    }
+    
+}
